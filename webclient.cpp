8de0cdd60d4be0e21b0bd0b842d50f658e7f3f0d
Srpak <95477134+Srpak@users.noreply.github.com>
2022-01-27 19:32:02 +0100

    Dont do this to me again

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,51 @@
 #include "webclient.h"
+#include <QtNetwork/QTcpSocket>
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
 }
+
+void Webclient::on_goButton_clicked() 
+{
+    m_hostname = hostnameInput->text();
+    if (m_hostname != "") 
+    {
+        unsigned short port = 80;
+        m_socket= new QTcpSocket(this);
+        connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+        connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::readyRead);
+        m_socket->connectToHost(m_hostname, port);
+
+        if (!m_socket->waitForConnected(5000)) 
+        {
+            outputTextbox->setText("Verbindung fehlgeschlagen!");
+        }
+    }
+    else 
+    {
+        outputTextbox->setText("Hostname eingeben!");
+    }
+}
+
+void Webclient::connected() 
+{
+    outputTextbox->setText("Verbunden\n");
+    this->m_socket->write("GET / HTTP/1.1\r\nHost:" + m_hostname.toLocal8Bit());
+}
+
+void Webclient::readyRead() 
+{
+    QString output = QString(m_socket->readAll());
+    while (output.at(output.length() - 1) == "\xa") 
+    {
+        output.resize(output.length() - 2);
+    }
+   
+    outputTextbox->append(output);
+    m_socket->disconnectFromHost();
+    outputTextbox->append("\nVerbindung getrennt");
+}
+
+
--- webclient.h
+++ webclient.h
@@ -3,12 +3,29 @@
 
 #include "ui_webclient.h"
 
+#include <QObject>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+
 class Webclient : public QWidget, private Ui::Webclient
 {
     Q_OBJECT
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+    void on_goButton_clicked();
+    void on_hostnameInput_returnPressed();
+
+public slots:
+    void connected();
+    void readyRead();
+
+private:
+    QTcpSocket *m_socket;
+    QString m_hostname;
 };
 
 #endif // WEBCLIENT_H
+
